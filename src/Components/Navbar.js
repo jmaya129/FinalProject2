feafe6c1502f08772f0b85bccf6f20bf493b2146
jmaya129 <89950080+jmaya129@users.noreply.github.com>
2022-04-06 12:26:20 -0400

    project day 5

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,25 +2,22 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div className="Navbar">
-      <nav class="navbar navbar-light bg-light">
-        <div class="container-fluid">
-          <form class="d-flex">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-
-            <button onclick="getElementById('demo').innerHTML=data()">
-              Search
-            </button>
-            <p id="demo"></p>
-          </form>
-        </div>
-      </nav>
-    </div>
+    <nav class="navbar navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand"> </a>
+        <form class="d-flex">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success" type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+    </nav>
   );
 }
 
