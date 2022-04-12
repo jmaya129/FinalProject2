f49b446a482f676994b2c04189fd19d9f234b70c
jmaya129 <89950080+jmaya129@users.noreply.github.com>
2022-04-12 12:27:19 -0400

    Api

--- src/App.js
+++ src/App.js
@@ -8,12 +8,32 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+    data: [];
   }
+  componentDidMount = () => {
+    fetch(
+      "https://data.cityofnewyork.us/resource/uvpi-gqnh.json?$$app_token=EhwYR2nqCIBmEXdrvrs8D3FpE"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ data: data });
+      })
+      .catch((e) => {
+        alert(e);
+      });
+  };
 
   render() {
     return (
       <div className="App">
         <h1>Tree Finder</h1>
+        <section className="list">{/* PUT API RESPONSE DATA HERE */}</section>
+        {this.state.data.map((application) => (
+          <li>
+            {application.app_no}: {application.other_requirements}
+          </li>
+        ))}
+
         <Navbar />
       </div>
     );
