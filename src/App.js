e88a5447b4adcc4605a9495a8eab51397689d076
jmaya129 <89950080+jmaya129@users.noreply.github.com>
2022-05-02 10:38:10 -0400

    trying to make the navbar pretty

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
-import Navbar from "./Components/Navbar";
+import results from "./Components/pages/results";
+import Navbar from "./Components/Navbar/Navbar";
 
 class App extends Component {
   constructor(props) {
@@ -40,6 +41,7 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar data={this.state.data} />
+        <results />
       </div>
     );
   }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,13 +1,41 @@
 import React from "react";
+import results from "/src/Components/pages/results";
+import "/src/Components/css/Navbar.css";
 
 function Navbar(props) {
   console.log(props);
+
+  // When the user scrolls the page, execute myFunction
+  window.onscroll = function () {
+    myFunction();
+  };
+
+  // Get the navbar
+  var navbar = document.getElementById("Navbar");
+
+  // Get the offset position of the navbar
+  var sticky = Navbar.offsetTop;
+
+  // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky");
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
 
   const findData = () => {
     for (let i = 0; i < props.data.length; i++) {
       console.log(props.data[i].spc_common);
     }
   };
+
+  function results(e) {
+    e.preventDefault();
+    window.location = "my-app/src/Components/results";
+  }
+
   return (
     <nav class="navbar navbar-light bg-light">
       <div class="container-fluid">
